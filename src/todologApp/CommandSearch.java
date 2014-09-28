13357da4c22d8967d35658ed53cd7db58301ee9f
vani gupta <gupta.vani1595@gmail.com>
2014-09-28 10:37:38 +0800

    added skeleton files for CommandDelete,CommandEdit,CommandMarkAsDone,CommandSearch

---
+++ src/todologApp/CommandSearch.java
@@ -0,0 +1,17 @@
+package todologApp;
+
+public class CommandSearch implements Command{
+	private static Task _task;
+	private static Storage _storage;
+	public CommandSearch(Task task) {
+		_task = task;
+	}
+	public void execute() {
+		// TODO Auto-generated method stub
+	}
+
+	public void undo() {
+		// TODO Auto-generated method stub
+	}
+
+}
