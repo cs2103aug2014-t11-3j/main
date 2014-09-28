ada7400f1557c34d13b5f1afee40c14524c82b57
stanley92 <Stanley92@hotmail.com>
2014-09-28 11:48:29 +0800

    Added more cases for testing

--- src/todologApp/ControllerTest.java
+++ src/todologApp/ControllerTest.java
@@ -8,12 +8,17 @@ public class ControllerTest {
 
 	@Test
 	public void testGetFirstWord() {
-		assertEquals("first line should be add", "add", Controller.getFirstWord("add group meeting"));
+		assertEquals("first line should be add", "add", 
+				Controller.getFirstWord("add group meeting"));
+		assertEquals("first line should be add", "add", 
+				Controller.getFirstWord("add \"group meeting\""));
 	}
 
 	@Test
 	public void testGetTheRestOfTheString() {
 		assertEquals("first line should be group meeting", "group meeting", 
 				Controller.getTheRestOfTheString("add group meeting"));
+		assertEquals("first line should be group meeting", "\"group meeting\"", 
+				Controller.getTheRestOfTheString("add \"group meeting\""));
 	}
 }
