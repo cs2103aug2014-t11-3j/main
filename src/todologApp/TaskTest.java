d54262de353ba205f5ff2c63c4d0ca9f2c348893
stanley92 <Stanley92@hotmail.com>
2014-09-28 13:36:59 +0800

    Edited Junit testing

--- src/todologApp/TaskTest.java
+++ src/todologApp/TaskTest.java
@@ -11,5 +11,10 @@ public class TaskTest {
 		assertEquals("first line should be group meeting", "group meeting", 
 				Task.parseName("\"group meeting\""));
 	}
+	public void testTaskParsing() {
+		Task task = new Task("group meeting");
+		assertEquals("aaa",TaskType.FLOATING,task.getTaskType());
+		assertEquals("bbb","group meeting",task.getName());
+	}
 
 }
