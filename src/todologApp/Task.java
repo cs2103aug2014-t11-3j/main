f855f0f26d55bba9458ea12e43dbff887d5b4d63
stanley92 <Stanley92@hotmail.com>
2014-09-29 19:03:37 +0800

    Added code to determine the day added for task

--- src/todologApp/Task.java
+++ src/todologApp/Task.java
@@ -20,25 +20,25 @@ public class Task {
 	private static String KEYWORD_HOURS = "hrs";
 
 	//DAY KEYWORDS
-	private static String DAY_KEYWORD_TODAY = "today";
-	private static String DAY_KEYWORD_TOMORROW = "tomorrow";
+	private static String DAY_KEYWORD_TODAY = "Today";
+	private static String DAY_KEYWORD_TOMORROW = "Tomorrow";
 	private static String DAY_KEYWORD_TMR = "tmr";
-	private static String DAY_KEYWORD_MONDAY = "monday";
+	private static String DAY_KEYWORD_MONDAY = "Monday";
 	private static String DAY_KEYWORD_MON = "mon";
-	private static String DAY_KEYWORD_TUESDAY = "tuesday";
+	private static String DAY_KEYWORD_TUESDAY = "Tuesday";
 	private static String DAY_KEYWORD_TUES = "tues";
 	private static String DAY_KEYWORD_TUE = "tue";
-	private static String DAY_KEYWORD_WEDNESDAY = "wednesday";
+	private static String DAY_KEYWORD_WEDNESDAY = "Wednesday";
 	private static String DAY_KEYWORD_WED = "wed";
-	private static String DAY_KEYWORD_THURSDAY = "thursday";
+	private static String DAY_KEYWORD_THURSDAY = "Thursday";
 	private static String DAY_KEYWORD_THURS = "thurs";
 	private static String DAY_KEYWORD_THUR = "thur";
 	private static String DAY_KEYWORD_THU = "thu";
-	private static String DAY_KEYWORD_FRIDAY = "friday";
+	private static String DAY_KEYWORD_FRIDAY = "Friday";
 	private static String DAY_KEYWORD_FRI = "fri";
-	private static String DAY_KEYWORD_SATURDAY = "saturday";
+	private static String DAY_KEYWORD_SATURDAY = "Saturday";
 	private static String DAY_KEYWORD_SAT = "sat";
-	private static String DAY_KEYWORD_SUNDAY = "sunday";
+	private static String DAY_KEYWORD_SUNDAY = "Sunday";
 	private static String DAY_KEYWORD_SUN = "sun";
 
 	private static String _name;
@@ -53,6 +53,8 @@ public class Task {
 		int lastIndex = parameter.lastIndexOf(QUOTATION_MARK);
 		if (parameter.length() == lastIndex+1) {
 			return (TaskType.FLOATING);
+		} else if (parameter.length() > lastIndex+2) {
+			return (TaskType.DEADLINE);
 		}
 		
 		return null;
@@ -61,6 +63,7 @@ public class Task {
 	private static String parseName(String parameter) {
 		int firstIndex = parameter.indexOf(QUOTATION_MARK);
 		int lastIndex = parameter.lastIndexOf(QUOTATION_MARK);
+		
 		if (lastIndex > firstIndex) {
 			String taskName = parameter.substring(firstIndex+1, lastIndex);
 			return taskName;
@@ -70,7 +73,34 @@ public class Task {
 			return INVALID_MESSAGE;
 		} else if (firstIndex == -1) {
 			return INVALID_MESSAGE;
+		} else {
+			String[] timeAndDay = parameter.split(" ", 3);
 		}
+
+		String taskName = parameter.substring(firstIndex+1, lastIndex);
+		String day = new String(timeAndDay[1]);
+		
+		if (day.equalsIgnoreCase(DAY_KEYWORD_TODAY)) {
+			return DAY_KEYWORD_TODAY;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_TOMORROW) || day.equalsIgnoreCase(DAY_KEYWORD_TMR)) {
+			return DAY_KEYWORD_TOMORROW;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_MONDAY) || day.equalsIgnoreCase(DAY_KEYWORD_MON)) {
+			return DAY_KEYWORD_MONDAY;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_TUESDAY) || day.equalsIgnoreCase(DAY_KEYWORD_TUE) || 
+					day.equalsIgnoreCase(DAY_KEYWORD_TUES)) {
+			return DAY_KEYWORD_TUESDAY;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_WEDNESDAY) || day.equalsIgnoreCase(DAY_KEYWORD_WED)) {
+			return DAY_KEYWORD_WEDNESDAY;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_THURSDAY) || day.equalsIgnoreCase(DAY_KEYWORD_THURS) || 
+					day.equalsIgnoreCase(DAY_KEYWORD_THUR) || day.equalsIgnoreCase(DAY_KEYWORD_THU)) {
+			return DAY_KEYWORD_THURSDAY;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_FRIDAY) || day.equalsIgnoreCase(DAY_KEYWORD_FRI)) {
+			return DAY_KEYWORD_FRIDAY;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_SATURDAY) || day.equalsIgnoreCase(DAY_KEYWORD_SAT)) {
+			return DAY_KEYWORD_SATURDAY;
+		} else if (day.equalsIgnoreCase(DAY_KEYWORD_SUNDAY) || day.equalsIgnoreCase(DAY_KEYWORD_SUN)) {
+			return DAY_KEYWORD_SUNDAY;
+		} 
 		
 		return null;
 	}
