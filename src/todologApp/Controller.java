1aff84507ee0dff0629790cd76e45676e769498c
stanley92 <Stanley92@hotmail.com>
2014-09-29 16:26:36 +0800

    Edited createCommand method

--- src/todologApp/Controller.java
+++ src/todologApp/Controller.java
@@ -18,23 +18,24 @@ public class Controller {
 		//...
 		command.undo();
 	}
-	private static Command createCommand(String userCommand) {
+	public static Command createCommand(String userCommand) {
 		String firstWord = getFirstWord(userCommand);
 		String restOfTheString = getTheRestOfTheString(userCommand);
 		if (firstWord.equalsIgnoreCase("add")) {
 			Task task = new Task(restOfTheString);
-			CommandAdd command = new CommandAdd(task);	
+			CommandAdd command = new CommandAdd(task);
+			return command;
 		} else if (firstWord == "delete")
-
 			return null;
 		return null;
 	}
+	
 	public static String getTheRestOfTheString(String userCommand) {
 		String[] result = userCommand.split(" ", 2);
 		String restOfTheWord = result[1];
 		return restOfTheWord;
 	}
-	
+
 	public static String getFirstWord(String userCommand) {
 		String[] result = userCommand.split(" ", 2);
 		String firstWord = result[0];
