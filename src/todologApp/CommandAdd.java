a193751486c4d08d9d331b31fe30f78edeb8fd4f
vani gupta <gupta.vani1595@gmail.com>
2014-09-30 11:51:46 +0800

    made changes in add function

--- src/todologApp/CommandAdd.java
+++ src/todologApp/CommandAdd.java
@@ -1,14 +1,18 @@
 package todologApp;
 
+import java.util.LinkedList;
+
 public class CommandAdd implements Command{
 	private static Task _task;
-	private static Storage _storage;
+	private static DBStorage _storage;
 	public CommandAdd(Task task) {
 		_task = task;
 	}
 	public void execute() {
 		_storage = Controller.getStorage();
-		_storage.store(_task);
+		LinkedList<Task> newList = _storage.load();
+		newList.add(_task);
+		_storage.store(newList);
 	}
 
 	public void undo() {
