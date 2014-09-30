6897308d93aed16b87f3489917c60e5eda11220a
stanley92 <Stanley92@hotmail.com>
2014-09-30 13:00:58 +0800

    Added new test case to determine types of task

--- src/todologApp/TaskTest.java
+++ src/todologApp/TaskTest.java
@@ -19,12 +19,18 @@ public class TaskTest {
 	@Test
 	public void testParseType() {
 		Task task = new Task ("\"group meeting\"");
-		assertEquals("output should be floating", TaskType.FLOATING , task.getTaskType());
+		assertEquals("output should be FLOATING", TaskType.FLOATING , task.getTaskType());
+		Task task2 = new Task ("\"group meeting\" from friday to saturday");
+		assertEquals("output should be TIMED", TaskType.TIMED , task2.getTaskType());
+		Task task3 = new Task ("\"group meeting\" by friday");
+		assertEquals("output should be DEADLINE", TaskType.DEADLINE , task3.getTaskType());
+		Task task4 = new Task ("\"group meeting\" every friday");
+		assertEquals("output should be RECURRING", TaskType.RECURRING , task4.getTaskType());
 	}
 
 	@Test
 	public void testParseDay() {
 		Task task = new Task ("\"group meeting\" fri 2359");
 		assertEquals("output should be Friday", "Friday" , task.getTaskDay());
-	}
+		}
 }
