b120c942b0483a0dc936eaa4f7501c380f0684b9
vani gupta <gupta.vani1595@gmail.com>
2014-10-13 12:13:18 +0800

    rename file for pushing to team member

---
+++ src/todologApp/CommandAddTes.java
@@ -0,0 +1,14 @@
+package todologApp;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CommandAddTes {
+	@Test
+	public void testExecute() throws Exception {
+		Task task = new Task("\"group meeting\"");
+		CommandAdd command = new command(task);
+		assertEquals("Description", "Added group meeting to ToDoLog",
+				command.execute());
+	}
+}
