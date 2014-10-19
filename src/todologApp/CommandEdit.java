8067e83784c39b5d0ac7b2d4f4e83d8c6f996221
vani gupta <gupta.vani1595@gmail.com>
2014-10-19 15:45:40 +0800

    CommandEdit. Editting all the fields.

--- src/todologApp/CommandEdit.java
+++ src/todologApp/CommandEdit.java
@@ -6,38 +6,89 @@ import java.util.LinkedList;
 public class CommandEdit implements Command {
 	private Task _taskExisting;
 	private String _toBeEdited;
+	private String _editType;
 	private Task _taskEdited;
 	private int index;
-	private String editType;
 	private DBStorage _storage;
 
 	// private static Storage _storage;
 
-	public CommandEdit(Task taskExisting, String toBeEdited) {
+	public CommandEdit(Task taskExisting, String toBeEdited, String editType) {
 		_taskExisting = taskExisting;
 		_toBeEdited = toBeEdited;
+		_editType = editType;
 		_taskEdited = _taskExisting;
 		_storage = Controller.getDBStorage();
-		editType = formNewTask();
+		
 	}
 
 	private String formNewTask() {
-		if (_toBeEdited.startsWith("\"") && _toBeEdited.endsWith("\"")) {
-			_taskEdited.setTaskName(_toBeEdited.substring(1,
-					_toBeEdited.length() - 1));
+		//change the name
+		if (_editType.equalsIgnoreCase("Task Name")||_editType.equalsIgnoreCase("Name")) {
+			_taskEdited.setTaskName(_toBeEdited);
 			return "name";
 		}
-		return "nothing";
-		// else if(equalsWeekDay(_toBeEdited)){
-		// _taskEdited.setDay(_toBeEdited);
-		// }
-		// else if(_toBeEdited.length()==4){
-		// _taskEdited.setTime(_toBeEdited);
-		// }
+		
+		//change Start day
+		 else if(_editType.equalsIgnoreCase("Start Day")||_editType.equalsIgnoreCase("Day")){
+			 if(equalsWeekDay(_toBeEdited)){
+				 _taskEdited.setStartDay(_toBeEdited);
+				 return "Start Day";
+			 }
+			 else{
+				 return "invalid";
+			 }
+		 }
+		//change End day
+		 else if(_editType.equalsIgnoreCase("End Day")||_editType.equalsIgnoreCase("Day")){
+			 if(equalsWeekDay(_toBeEdited)){
+				 _taskEdited.setEndDay(_toBeEdited);
+				 return "End Day";
+			 }
+			 else{
+				 return "invalid";
+			 }
+		 }
+		//change Start Time
+		 else if(_editType.equalsIgnoreCase("Start Time")){
+			 if(_toBeEdited.length()==4){
+				 _taskEdited.setStartTime(_toBeEdited);
+			 	return "Start time";
+			 }
+			 else{
+				 return "invalid";
+			 }
+		 }
+		//change End Time
+		 else if(_editType.equalsIgnoreCase("End Time")){
+			 if(_toBeEdited.length()==4){
+				 _taskEdited.setEndTime(_toBeEdited);
+			 	return "End time";
+			 }
+			 else{
+				 return "invalid";
+			 }
+		 }
+		
+		//changeVenue
+		 else if(_editType.equalsIgnoreCase("Venue")){
+			 _taskEdited.setVenue(_toBeEdited);
+			 return "venue";
+		 }
+		//change person
+		 else if(_editType.equalsIgnoreCase("person")){
+			 _taskEdited.setPerson(_toBeEdited);
+			 return "person";
+		 }
+		 else{
+			 return "invalid";
+		 }
 	}
 
 	public String execute() {
 		String feedback;
+		String editedField;
+		editedField=formNewTask();
 		LinkedList<Task> tasks = _storage.load();
 		index = tasks.indexOf(_taskExisting);
 		tasks.remove(index);
@@ -48,7 +99,12 @@ public class CommandEdit implements Command {
 			feedback = "Cannot store the list to ToDoLog";
 			return feedback;
 		}
-		feedback = "Edited " + editType + " of the task.";
+		if(editedField.equals("invalid")){
+			feedback = "Invalid input";
+		}
+		else{
+			feedback = "Edited " + editedField + " of the task.";	
+		}
 		return feedback;
 	}
 
@@ -56,6 +112,7 @@ public class CommandEdit implements Command {
 		String feedback;
 		LinkedList<Task> tasks = _storage.load();
 		tasks.remove(index);
+		formNewTask();
 		tasks.add(index, _taskExisting);
 		feedback = "Undone the edit command.";
 		return feedback;
@@ -67,7 +124,9 @@ public class CommandEdit implements Command {
 				|| day.equalsIgnoreCase("wednesday")
 				|| day.equalsIgnoreCase("thursday")
 				|| day.equalsIgnoreCase("friday")
-				|| day.equalsIgnoreCase("saturday")) {
+				|| day.equalsIgnoreCase("saturday")
+				|| day.equalsIgnoreCase("today")
+				|| day.equalsIgnoreCase("tomorrow")) {
 			return true;
 		} else {
 			return false;
