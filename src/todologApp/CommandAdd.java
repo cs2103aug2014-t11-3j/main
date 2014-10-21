beefc2e6ad4f6f9dcfff7f6f5831ec35c089a5bb
vani gupta <gupta.vani1595@gmail.com>
2014-10-21 21:39:06 +0800

    added sortByDate method so that it sorts when the task is added.

--- src/todologApp/CommandAdd.java
+++ src/todologApp/CommandAdd.java
@@ -15,7 +15,8 @@ public class CommandAdd implements Command {
 		_storage= Controller.getDBStorage();
 		LinkedList<Task> newList = new LinkedList<Task>();
 		newList=_storage.load();
-		newList.add(_task);
+		sortByDate(newList);
+		//newList.add(_task);
 		try {
 			_storage.store(newList);
 		} catch (IOException e) {
@@ -24,6 +25,55 @@ public class CommandAdd implements Command {
 		}
 		feedback = "Added " + _task.getTaskName() + " to ToDoLog";
 		return feedback;
+	}
+	
+	public void sortByDate(LinkedList<Task> newList){
+	    if(_task.getTaskType().equals("FLOATING")){
+	    	newList.add(0,_task);
+	    }
+	    
+	    
+	    else{
+		for(int i=0;i<newList.size()-1;i++){
+			//if the date is to be inserted between two years
+			if((newList.get(i).getEndYear()<_task.getEndYear())&&(newList.get(i+1).getEndYear()>_task.getEndYear())){
+				newList.add(i+1,_task);
+			}
+			
+			
+			//
+			else if(newList.get(i).getEndYear()==_task.getEndYear()&&newList.get(i+1).getEndYear()>_task.getEndYear()){
+				if(newList.get(i).getEndMonth()<_task.getEndMonth()){
+					newList.add(i,_task);
+				}
+				else if(newList.get(i).getEndMonth()==_task.getEndMonth()){
+					if((newList.get(i).getEndDate()<=_task.getEndDate())&&(newList.get(i+1).getEndDate()>=_task.getEndDate())){
+						newList.add(i+1,_task);
+					}
+				}
+				else{
+					newList.add(i,_task);	
+				}	
+			}
+			
+			//when both years are equal, the difference can be by month or date
+			else if(newList.get(i).getEndYear()==_task.getEndYear()&&newList.get(i+1).getEndYear()==_task.getEndYear()){
+				if((newList.get(i).getEndMonth()<_task.getEndMonth())&&(newList.get(i+1).getEndMonth()>_task.getEndMonth())){
+					newList.add(i+1,_task);
+					}
+				else if(newList.get(i).getEndMonth()==_task.getEndMonth()&&newList.get(i+1).getEndMonth()==_task.getEndMonth()){
+					if((newList.get(i).getEndDate()<=_task.getEndDate())&&(newList.get(i+1).getEndDate()>=_task.getEndDate())){
+					newList.add(i+1,_task);
+						}
+				}
+			}
+			
+			//add towards the end
+			else{
+				newList.add(_task);	
+			}
+		}
+	}
 	}
 
 	public String undo() {
