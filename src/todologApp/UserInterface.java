1948119f8d335978d88cb1aee4b96c8f5e910327
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2014-10-31 17:42:33 +0800

    change icon in file and add tray (not tested)

--- src/todologApp/UserInterface.java
+++ src/todologApp/UserInterface.java
@@ -508,11 +508,11 @@ public class UserInterface extends JFrame implements WindowListener {
 		ImageIcon img = new ImageIcon("src/ToDoLog logo.gif");
 		this.setIconImage(img.getImage());
 		Application application = Application.getApplication();
-		Image image = Toolkit.getDefaultToolkit().getImage("src/ToDoLog logo.gif");
+		Image image = Toolkit.getDefaultToolkit().getImage("src/ToDoLog.gif");
 		application.setDockIconImage(image);
 		initialize(this); 
 		fillUpTheJFrame(this);
-		
+		makeTrayIcon(this);
 		Controller.init();
 		toDoListItems = Controller.getDBStorage().load();
 		toDoListTableModel = new ToDoListTableModel(toDoListItems);
@@ -524,6 +524,52 @@ public class UserInterface extends JFrame implements WindowListener {
 		// create more here
 	}
 	
+	private void makeTrayIcon(JFrame userInterface) {
+		//Check the SystemTray is supported
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+        ImageIcon img = new ImageIcon("src/ToDoLog logo.gif");
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon =
+                new TrayIcon(img.getImage());
+        final SystemTray tray = SystemTray.getSystemTray();
+       
+        // Create a pop-up menu components
+        MenuItem aboutItem = new MenuItem("About");
+        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+        Menu displayMenu = new Menu("Display");
+        MenuItem errorItem = new MenuItem("Error");
+        MenuItem warningItem = new MenuItem("Warning");
+        MenuItem infoItem = new MenuItem("Info");
+        MenuItem noneItem = new MenuItem("None");
+        MenuItem exitItem = new MenuItem("Exit");
+       
+        //Add components to pop-up menu
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(cb1);
+        popup.add(cb2);
+        popup.addSeparator();
+        popup.add(displayMenu);
+        displayMenu.add(errorItem);
+        displayMenu.add(warningItem);
+        displayMenu.add(infoItem);
+        displayMenu.add(noneItem);
+        popup.add(exitItem);
+       
+        trayIcon.setPopupMenu(popup);
+       
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+        }
+		
+	}
+
 	// remember to write unit test as you code
 	private class CommandEntryTextFieldActionListener implements ActionListener{
 		@Override
