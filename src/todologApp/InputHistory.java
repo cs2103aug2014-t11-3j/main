24278b1db01ceeca5552652fcf75dc6f824e8522
stanley92 <Stanley92@hotmail.com>
2014-11-08 17:39:32 +0800

    edited the new class

--- src/todologApp/InputHistory.java
+++ src/todologApp/InputHistory.java
@@ -1,5 +1,43 @@
 package todologApp;
 
-public class InputHistory {
+import java.util.LinkedList;
 
+public class InputHistory {
+	private LinkedList<String> _String;
+	private int _position;
+	public InputHistory() {
+		_String = new LinkedList<String>();
+		_position = -1;
+	}
+	public Command getBackwards() throws Exception {
+		if (_position >= 0) {
+			return _commands.get(_position);
+		} else {
+			return null;
+		}
+	}
+	public void goBackwards() throws Exception {
+		if (_position >= 0) {
+			_position--;
+		} else {
+			throw new Exception("No commands to undo!");
+		}
+	}
+	public Command getForwards() throws Exception {
+		if (_position < _commands.size()) {			
+			Command toBeRedone = _commands.get(_position+1);
+			return toBeRedone;
+		} else {
+			_position--;
+			return null;
+		}
+	}
+	public void goForwards() throws Exception {
+		_position++;
+		if (_position >= _commands.size()) {
+			_position--;
+			throw new Exception("No commands to redo!");
+		}
+	}
+	
 }
