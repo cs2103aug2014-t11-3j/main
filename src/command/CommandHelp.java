41107599629ef9541db21fc8b4cc9b412187d45e
stanley92 <Stanley92@hotmail.com>
2014-11-10 23:28:38 +0800

    help text fix

--- src/command/CommandHelp.java
+++ src/command/CommandHelp.java
@@ -2,12 +2,19 @@ package command;
 
 import gui.HelpFrame;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Scanner;
+
+import logger.Log;
 
 public class CommandHelp implements Command {
 	
+	private static final String FILEPATH_HELPTEXT = "resources/text/helper.txt";
+
 	private Scanner inFile;
 	
 	private static final String FEEDBACK_HELP = "Help is here!";
@@ -18,14 +25,17 @@ public class CommandHelp implements Command {
 	public String execute() {
 		String inFileText = "";
 		try {
-			inFile = new Scanner( new FileReader("helper.txt"));
+			URL helpTextUrl = this.getClass().getClassLoader().getResource(FILEPATH_HELPTEXT);
+			inFile = new Scanner(new BufferedReader(new InputStreamReader(helpTextUrl.openStream())));
 			while(inFile.hasNextLine() != false ) {
 				String line = inFile.nextLine();
 				inFileText = inFileText + line + "\n"; 
 			} 	
 		} catch (FileNotFoundException e) {
-				e.printStackTrace();
-		}	
+			Log.error("helper.txt not found");
+		} catch (IOException e1){
+			Log.error("error reading helper.txt");
+		}
 		if (inFileText != null ) {
 			HelpFrame generateHelpText = new HelpFrame(inFileText);
 			generateHelpText.execute();
