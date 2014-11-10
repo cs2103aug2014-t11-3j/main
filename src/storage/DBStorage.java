fe1257463ee7f8ee3fec24ef528a1520aa8ae82a
stanley92 <Stanley92@hotmail.com>
2014-11-10 20:57:44 +0800

    Added @Author for DBStorage Class

--- src/storage/DBStorage.java
+++ src/storage/DBStorage.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 
 import common.Task;
 
+//@Author A0112156U
 public class DBStorage implements Storage {
 	private LinkedList<Task> _tasks;
 	private FileStorage _fileStorage;
